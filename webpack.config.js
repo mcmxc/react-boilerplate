c6003dbf39586dd42bd0488e67d37d996d8ed4f8
voloshchenko <ovoloshchenko@cogniance.com>
2019-11-24 01:42:15 +0200

    babel and babel loader added

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist'),
     filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    ],
   },
 };
