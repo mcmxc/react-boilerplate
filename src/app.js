ca8fbbbc3bf37b3d2294cdda2e3461eefa38da3f
voloshchenko <ovoloshchenko@cogniance.com>
2019-11-24 11:24:09 +0200

    hot loader added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
 
 function App() {
   return <h1>App</h1>;
 }
 
-export default App;
+export default hot(module)(App);
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,22 @@
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      reportFilename: 'bundle_sizes.html',
+    }),
+  ],
+  /* Example of how to exclude react/react-dom (or any other library) from bundle
+     if you want to use them via CDN Links
+     (which should be added to index.html)
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  },
+  */
 });
